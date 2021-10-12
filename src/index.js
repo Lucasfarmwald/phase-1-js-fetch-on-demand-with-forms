ba9f76d6c83f0f3e5bdb074f71adef6ea24ae86c
lucasfarmwald <farmwaldlucas@gmail.com>
2021-10-11 19:44:12 -0500

    commit tree 307f8773378e4c7b6fd06240c889b9e18da98d26

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ const init = () => {
   
 }
 
+const formData = {
+    Name: "lucas",
+    Email: "farmwaldlucas@gmail.com",
+  };
+
+
+
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+
+ async function submitData(){
+     const response = await fetch("http://localhost:3000/users", configurationObject);
+     const object = response.JSON;
+}
 document.addEventListener('DOMContentLoaded', init);
